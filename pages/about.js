2322b7529ac2671960d4a74f2792da8a6964727b
Noah Farkas <noahfarkas@MacBook-Pro-2.local>
2022-03-09 13:03:48 -0800

    air table form

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import react from'react'
+import Layout from '../components/layout/Layout';
+
+function about() {
+    return (
+    <Layout>
+        <h1>about</h1> 
+        <>This is my website. This is made in Vscode and is a projects that I have made</>
+        </Layout>
+    )
+}
+
+export default about;
